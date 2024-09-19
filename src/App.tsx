d626f241069265c03c6b82bea1f48be1a5a215f4
Hyoeunkh <lhe4528@knu.ac.kr>
2024-09-19 23:32:47 +0900

    design: globalstyle 설정

--- src/App.tsx
+++ src/App.tsx
@@ -8,17 +8,22 @@ import MainPage from '@/pages/Main';
 import MapPage from '@/pages/Map';
 import MyPage from '@/pages/My';
 
+import GlobalStyle from './global';
+
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<MainLayout />}>
-        <Route index path="/" element={<MainPage />} />
-        <Route path="/influencer" element={<InfluencerPage />} />
-        <Route path="/map" element={<MapPage />} />
-        <Route path="/detail" element={<DetailPage />} />
-        <Route path="/my" element={<MyPage />} />
-      </Route>
-    </Routes>
+    <>
+      <GlobalStyle />
+      <Routes>
+        <Route path="/" element={<MainLayout />}>
+          <Route index path="/" element={<MainPage />} />
+          <Route path="/influencer" element={<InfluencerPage />} />
+          <Route path="/map" element={<MapPage />} />
+          <Route path="/detail" element={<DetailPage />} />
+          <Route path="/my" element={<MyPage />} />
+        </Route>
+      </Routes>
+    </>
   );
 }
 
