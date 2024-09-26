75143044cfb566bb4b7b0becd260a833ce646db0
Hyoeunkh <lhe4528@knu.ac.kr>
2024-09-26 22:22:59 +0900

    feat: 유튜브 썸네일 추출 로직 구현

--- src/components/features/Main/SpotSection/SpotItem.tsx
+++ src/components/features/Main/SpotSection/SpotItem.tsx
@@ -8,9 +8,17 @@ import { Paragraph } from '@/components/common/typography/Paragraph';
 import { SpotData } from '@/types';
 
 export default function SpotItem({ videoId, videoAlias, videoUrl, place }: SpotData) {
+  function extractYoutubeVideoId(url: string) {
+    const match = url.match(/(?:https?:\/\/)?(?:www\.)?youtu(?:be\.com\/watch\?v=|\.be\/)([\w-]*)(&(amp;)?[\w?=]*)?/);
+    return match && match[1] ? match[1] : null;
+  }
+
   return (
     <Wrapper to={`/detail/${place.placeId}`}>
-      <Image src={videoUrl} alt={String(videoId)} />
+      <Image
+        src={`https://img.youtube.com/vi/${extractYoutubeVideoId(videoUrl)}/maxresdefault.jpg`}
+        alt={String(videoId)}
+      />
       <Paragraph size="l" weight="bold" variant="white">
         {videoAlias}
       </Paragraph>
