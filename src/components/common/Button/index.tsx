5754ee9094b17374d08b7bdf0f5d5a6d85ceab75
Hyoeunkh <lhe4528@knu.ac.kr>
2024-09-27 20:29:50 +0900

    refactor: button 컴포넌트 리팩토링

--- src/components/common/Button/index.tsx
+++ src/components/common/Button/index.tsx
@@ -9,96 +9,101 @@ export default function Button({ ...props }: Props) {
   return <Wrapper {...props} />;
 }
 
-const Wrapper = styled.button<Pick<Props, 'theme' | 'size'>>(
-  {
-    width: '100%',
-    borderRadius: '6px',
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    cursor: 'pointer',
-    transition: 'background-color 200ms',
-    border: 'none',
-  },
-  ({ size = 'responsive' }) => {
-    const largeStyle = {
-      height: '60px',
-      fontSize: '22px',
-    };
+const baseButtonStyle = {
+  width: '100%',
+  borderRadius: '6px',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  cursor: 'pointer',
+  transition: 'background-color 200ms',
+  border: 'none',
+};
 
-    const smallStyle = {
-      height: '46px',
-      fontSize: '18px',
-    };
-    if (size === 'large') {
-      return largeStyle;
-    }
+const sizeStyles = (size: Props['size'] = 'responsive') => {
+  const largeStyle = {
+    height: '60px',
+    fontSize: '22px',
+  };
 
-    if (size === 'small') {
-      return smallStyle;
-    }
+  const smallStyle = {
+    height: '46px',
+    fontSize: '18px',
+  };
 
+  if (size === 'large') {
+    return largeStyle;
+  }
+
+  if (size === 'small') {
+    return smallStyle;
+  }
+
+  return {
+    ...smallStyle,
+    [`@media screen and (min-width:1100px)`]: largeStyle,
+  };
+};
+
+const themeStyles = (theme: Props['theme'] = 'mint') => {
+  if (theme === 'outline') {
     return {
-      ...smallStyle,
-      [`@media screen and (min-width:1100px)`]: {
-        ...largeStyle,
-      },
-    };
-  },
-  ({ theme = 'mint' }) => {
-    if (theme === 'outline') {
-      return {
-        boxShadow: '0 0 0 1px #ffffff inset',
-        color: '#ffffff',
-        background: 'none',
-
-        '&:hover': {
-          backgroundColor: '#1b1a1a',
-        },
-      };
-    }
-
-    if (theme === 'white') {
-      return {
-        color: '#000',
-        backgroundColor: '#fff',
-
-        '&:hover': {
-          backgroundColor: '#f8f8f8',
-        },
-      };
-    }
-
-    if (theme === 'kakao') {
-      return {
-        color: '#111',
-        backgroundColor: '#fee500',
-
-        '&:hover': {
-          backgroundColor: '#fada0a',
-        },
-      };
-    }
-
-    if (theme === 'visit') {
-      return {
-        color: '#55EBFF',
-        boxShadow: '0 0 0 1px #55EBFF inset',
-        background: 'none',
-
-        '&:hover': {
-          backgroundColor: '#1b1a1a',
-        },
-      };
-    }
-
-    return {
-      color: '#000',
-      backgroundColor: '#55EBFF',
+      boxShadow: '0 0 0 1px #ffffff inset',
+      color: '#ffffff',
+      background: 'none',
 
       '&:hover': {
-        backgroundColor: '#3da3b1',
+        backgroundColor: '#1b1a1a',
       },
     };
-  },
+  }
+
+  if (theme === 'white') {
+    return {
+      color: '#000',
+      backgroundColor: '#fff',
+
+      '&:hover': {
+        backgroundColor: '#f8f8f8',
+      },
+    };
+  }
+
+  if (theme === 'kakao') {
+    return {
+      color: '#111',
+      backgroundColor: '#fee500',
+
+      '&:hover': {
+        backgroundColor: '#fada0a',
+      },
+    };
+  }
+
+  if (theme === 'visit') {
+    return {
+      color: '#55EBFF',
+      boxShadow: '0 0 0 1px #55EBFF inset',
+      background: 'none',
+
+      '&:hover': {
+        backgroundColor: '#1b1a1a',
+      },
+    };
+  }
+
+  return {
+    color: '#000',
+    backgroundColor: '#55EBFF',
+
+    '&:hover': {
+      backgroundColor: '#3da3b1',
+    },
+  };
+};
+
+const Wrapper = styled.button<Pick<Props, 'theme' | 'size'>>(
+  baseButtonStyle,
+  ({ size }) => sizeStyles(size),
+  ({ theme }) => themeStyles(theme),
 );
