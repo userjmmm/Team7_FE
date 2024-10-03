e7060fdf3e58d6e33aba7b3e276fb3f20f835e4f
userjmmm <a@as-MacBook-Pro.local>
2024-10-03 16:33:58 +0900

    feat: MapWindow 위에 주변 찾기 버튼 생성

--- src/components/Map/MapWindow/index.tsx
+++ src/components/Map/MapWindow/index.tsx
@@ -1,10 +1,52 @@
+import { useState } from 'react';
 import { Map } from 'react-kakao-maps-sdk';
 
+import styled from 'styled-components';
+
+import Button from '@/components/common/Button';
+
 export default function MapWindow() {
+  const [map, setMap] = useState<kakao.maps.Map | null>(null);
   const center = {
     lat: 35.889062,
     lng: 128.610283,
   };
 
-  return <Map center={center} style={{ width: '100%', height: '570px' }} level={4} />;
+  const handleSearchNearby = () => {
+    // 변경 예정
+    if (map) {
+      const currentCenter = map.getCenter();
+      console.log('현재 중심 좌표:', currentCenter.getLat(), currentCenter.getLng());
+    }
+  };
+
+  return (
+    <MapContainer>
+      <ButtonContainer>
+        <Button
+          onClick={handleSearchNearby}
+          theme="mint"
+          size="responsive"
+          style={{ width: '160px', padding: '10px 20px', margin: '5px' }}
+        >
+          주변 찾기
+        </Button>
+      </ButtonContainer>
+      <Map center={center} style={{ width: '100%', height: '100%' }} level={4} onCreate={setMap} />
+    </MapContainer>
+  );
 }
+
+const MapContainer = styled.div`
+  position: relative;
+  width: 100%;
+  height: 570px;
+`;
+
+const ButtonContainer = styled.div`
+  position: absolute;
+  top: 30px;
+  left: 50%;
+  transform: translateX(-50%);
+  z-index: 10;
+`;
