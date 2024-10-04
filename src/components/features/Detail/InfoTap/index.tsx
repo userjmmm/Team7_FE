f4ea8b3ef4f40394ba4e4d7db7be0d2e84b3f9f6
Hyoeunkh <lhe4528@knu.ac.kr>
2024-10-04 19:00:50 +0900

    feat: msw를 사용한 DetailPage API 구현

--- src/components/features/Detail/InfoTap/index.tsx
+++ src/components/features/Detail/InfoTap/index.tsx
@@ -8,97 +8,35 @@ import FacilitySign from './FacilitySign';
 import MenuList from './MenuList';
 import MenuModal from './MenuModal';
 import OpenHour from './OpenHour';
+import { FacilityInfo, Menu, OpenHourData } from '@/types';
 
-const menuImgUrls = [
-  'https://via.placeholder.com/500',
-  'https://via.placeholder.com/500',
-  'https://via.placeholder.com/700',
-];
-const menuList = [
-  {
-    price: 14000,
-    recommend: false,
-    menu: '국내산 돼지 안심을 료코만의',
-  },
-  {
-    price: 3000,
-    recommend: false,
-    menu: '리조또',
-  },
-  {
-    price: 14000,
-    recommend: false,
-    menu: '국내산 돼지 안심을 료코만의 방식으로 숙성 및 조리하여 육즙과 부드러움의 특징을 살린 메뉴',
-  },
-  {
-    price: 3000,
-    recommend: false,
-    menu: '리조또',
-  },
-];
-const time = {
-  periodList: [
-    {
-      timeName: '영업시간',
-      timeSE: '10:00 - 22:00',
-      dayOfWeek: '월',
-    },
-    {
-      timeName: '영업시간',
-      timeSE: '10:00 - 22:00',
-      dayOfWeek: '화',
-    },
-    {
-      timeName: '영업시간',
-      timeSE: '10:00 - 22:00',
-      dayOfWeek: '수',
-    },
-    {
-      timeName: '영업시간',
-      timeSE: '10:00 - 22:00',
-      dayOfWeek: '목',
-    },
-    {
-      timeName: '영업시간',
-      timeSE: '10:00 - 22:00',
-      dayOfWeek: '금',
-    },
-  ],
-  offdayList: [
-    {
-      holidayName: '공휴일',
-      weekAndDay: '토',
-      temporaryHolidays: 'N',
-    },
-  ],
+type Props = {
+  facilityInfo: FacilityInfo;
+  openHour: OpenHourData;
+  menuInfos: {
+    menuImgUrls: [string];
+    menuList: [Menu];
+    timeExp: Date;
+  };
 };
-export default function InfoTap() {
+export default function InfoTap({ facilityInfo, openHour, menuInfos }: Props) {
   const [moreMenu, setMoreMenu] = useState(false);
   return (
     <Wrapper>
-      <FacilitySign
-        facilityInfo={{
-          wifi: true,
-          pet: true,
-          parking: true,
-          forDisabled: true,
-          nursery: true,
-          smokingRoom: true,
-        }}
-      />
+      <FacilitySign facilityInfo={facilityInfo} />
       <PeriodWrapper>
         <Paragraph size="m" weight="bold" variant="white">
           운영 시간
         </Paragraph>
-        <OpenHour openHour={time} />
+        <OpenHour openHour={openHour} />
       </PeriodWrapper>
       <MenuWrapper>
         <Paragraph size="m" weight="bold" variant="white">
           메뉴
         </Paragraph>
         <MenuContainer>
-          <MenuModal images={menuImgUrls} />
-          <MenuList lists={menuList.slice(0, moreMenu ? menuList.length : 4)} />
+          <MenuModal images={menuInfos.menuImgUrls} />
+          <MenuList lists={menuInfos.menuList.slice(0, moreMenu ? menuInfos.menuList.length : 4)} />
           <MoreMenuBtn onClick={() => setMoreMenu(!moreMenu)}>{moreMenu ? '메뉴 접기' : '메뉴 더보기'}</MoreMenuBtn>
         </MenuContainer>
       </MenuWrapper>
