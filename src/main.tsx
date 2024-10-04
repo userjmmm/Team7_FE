aa366fb472fb63d32269ba3cb3bf623a4dd2353d
Hyoeunkh <lhe4528@knu.ac.kr>
2024-10-04 18:55:04 +0900

    feat: msw 사용을 위한 세팅

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,20 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
+import { QueryClientProvider } from '@tanstack/react-query';
 import App from './App';
+import { queryClient } from './api/instance/index.js';
 
+if (import.meta.env.DEV) {
+  const { worker } = await import('./mocks/browser.js');
+  worker.start();
+}
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </QueryClientProvider>
   </StrictMode>,
 );
