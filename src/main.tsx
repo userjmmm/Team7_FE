75207e4b4b368c4fd452a9f26e3b55d699ded772
Hyoeunkh <lhe4528@knu.ac.kr>
2024-10-04 22:32:50 +0900

    fix: msw 비동기 처리

--- src/main.tsx
+++ src/main.tsx
@@ -3,19 +3,25 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
 import { QueryClientProvider } from '@tanstack/react-query';
-import App from './App';
+
 import { queryClient } from './api/instance/index.js';
 
-if (import.meta.env.DEV) {
-  const { worker } = await import('./mocks/browser.js');
-  worker.start();
+import App from './App';
+
+async function startApp() {
+  if (import.meta.env.DEV) {
+    const { worker } = await import('./mocks/browser.js');
+    await worker.start({ onUnhandledRequest: 'bypass' });
+  }
 }
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </QueryClientProvider>
-  </StrictMode>,
-);
+startApp().then(() => {
+  createRoot(document.getElementById('root')!).render(
+    <StrictMode>
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </QueryClientProvider>
+    </StrictMode>,
+  );
+});
