7dcc57bac13e5a637d6099a2ed0aebe783c320f8
Hyoeunkh <lhe4528@knu.ac.kr>
2024-10-10 02:36:08 +0900

    feat: 인플루언서 컴포넌트 호버링 효과 구현

--- src/components/Main/InfluencerSection/InfluencerItem.tsx
+++ src/components/Main/InfluencerSection/InfluencerItem.tsx
@@ -2,8 +2,9 @@ import { Link } from 'react-router-dom';
 
 import styled from 'styled-components';
 
+import { MdLocationOn } from 'react-icons/md';
 import { Paragraph } from '@/components/common/typography/Paragraph';
-
+import backCard from '@/assets/images/back-card.png';
 import { InfluencerData } from '@/types';
 
 export default function InfluencerItem({
@@ -14,7 +15,15 @@ export default function InfluencerItem({
 }: InfluencerData) {
   return (
     <Wrapper to={`/influencer/${influencerId}`}>
-      <Image src={influencerImgUrl} alt={influencerName} />
+      <ImageContainer>
+        <FrontImage src={influencerImgUrl} alt={influencerName} />
+        <BackImageWrapper>
+          <MdLocationOn size={50} color="#55EBFF" />
+          <Paragraph size="m" variant="white" weight="bold">
+            지도 보기
+          </Paragraph>
+        </BackImageWrapper>
+      </ImageContainer>
       <Paragraph size="m" weight="bold" variant="white">
         {influencerName}
       </Paragraph>
@@ -34,10 +43,45 @@ const Wrapper = styled(Link)`
   line-height: 30px;
 `;
 
-const Image = styled.img`
+const ImageContainer = styled.div`
   width: 168px;
   height: 208px;
+  position: relative;
+  border-radius: 6px;
+  transition: transform 0.8s;
+  transform-style: preserve-3d;
+
+  ${Wrapper}:hover & {
+    transform: rotateY(180deg);
+  }
+`;
+
+const FrontImage = styled.img`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  backface-visibility: hidden;
   object-fit: cover;
   margin-bottom: 8px;
+  border-radius: 6px;
+`;
+
+const BackImageWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  background-image: url(${backCard});
+  background-size: cover;
+  background-position: center;
+  transform: rotateY(180deg);
+  backface-visibility: hidden;
   border-radius: 6px;
 `;
